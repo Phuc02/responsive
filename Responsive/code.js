6072632fe4730f697f338714da8c172e660ef05d
Phuc02 <49591823+Phuc02@users.noreply.github.com>
2021-11-07 21:53:24 +0700

    Add files via upload

---
+++ Responsive/code.js
@@ -0,0 +1,4 @@
+var x = document.getElementsByTagName("input")[0];
+            x.onclick = function () {
+                document.getElementsByClassName("menu")[0].style.background = "red";
+            };
